cd6273be6a60cf302afc43dcbd86c9a62f065c4b
tangxiang <tangx@amyrobotics.com>
2018-07-16 18:39:49 +0800

    update

--- 1.0start/helloworld.cpp
+++ 1.0start/helloworld.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
 using namespace std;
+int __global;
 
 int main()
 {
 	cout<<"helloworld"<<endl;
+	// int i = {3.14};
+	// int i(3.14);
+	// int i{3};
+	// cout << i <<endl;
+	int _;
+	int _local;
+	int local_int;
+	double Double;
+	cout << local_int << endl;
+	int i=100;
+	cout << &i <<endl;
+	int *p = NULL;
+	cout << p <<endl;
+	cout << *p <<endl;
+	cout << &p <<endl;
 	return 0;
 }
