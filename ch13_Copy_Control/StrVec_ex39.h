d66e41234175b708ad9450ee93f1c51b1b7139bd
tangxiang <tangxiang007@gmail.com>
2018-10-09 07:39:30 -0700

    add StrVec_ex39.h

---
+++ ch13_Copy_Control/StrVec_ex39.h
@@ -0,0 +1,83 @@
+#include <string>
+#include <<utility>
+#include <memory>
+
+class StrVec
+{
+public:
+	StrVec() : elements(nullptr), first_free(nullptr), cap(nullptr) { }
+	StrVec(const StrVec&);
+	StrVec &operator=(const StrVec&);
+	~StrVec();
+	void push_back(const std::string&);
+	size_t size() const { return first_free - elements; }
+	size_t capacity() const { return cap - elements; }
+	std::string *begin() const { return elements; }
+	std::string *end() const { return first_free; }
+private:
+	static std::allocator<std::string> alloc;
+	void chk_n_alloc() { if(size() == capacity()) reallocate(); }
+	std::pair<std::string*, std::string*> alloc_n_copy(const std::string *, const std::string*);
+	void free();
+	void reallocate();
+	std::string *elements;
+	std::string *first_free;
+	std::string *cap;
+};
+
+void StrVec::push_back(std::string &s)
+{
+	chk_n_alloc();
+	alloc.construct(first_free++, s);
+}
+
+std::pair<std::string*,std::string*> StrVec::alloc_n_copy(const std::string *b, cosnt std::string *e)
+{
+	auto data = alloc.allocator(e-b);
+	return {data, uninitialized_copy(b, e, data)};
+}
+
+void StrVec::free()
+{
+	if(elements)
+	{
+		for(auto p = first_free; p != elements; )
+			alloc.destroy(--p);
+		alloc.deallocate(elements, cap-elements);
+	}
+}
+
+StrVec::StrVec(const StrVec &s)
+{
+	auto newdata = alloc_n_copy(s.begin(), s.end());
+	elements = newdata.first;
+	first_free = cap = newdata.second;
+}
+
+StrVec::~StrVec()
+{
+	free();
+}
+
+StrVec &StrVec::operator=(const StrVec &rhs)
+{
+	auto data = alloc_n_copy(rhs.begin(), rhs.end());
+	free();
+	elements = data.first;
+	first_free = cap = data.second;
+	return *this;
+}
+
+void StrVec::reallocate()
+{
+	auto newcapacity = size() ? 2 * size() : 1;
+	auto newdata = alloc.allocate(newcapacity);
+	auto dest = newdata;
+	auto elem = elements;
+	for(size_t i = 0; i != size(); ++i)
+		alloc.construct(dest++, std::move(*elem++));
+	free();
+	elements = newdata;
+	first_free = dest;
+	cap = elements + newcapacity;
+}
