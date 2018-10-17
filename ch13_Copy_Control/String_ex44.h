9fee91769057e2332242381fb8c108f22d809dfa
tangxiang <tangxiang007@gmail.com>
2018-10-17 08:30:46 -0700

    update String_ex44.h

--- ch13_Copy_Control/String_ex44.h
+++ ch13_Copy_Control/String_ex44.h
@@ -1,9 +1,11 @@
 #include <memory>
+#include <algorithm>
 
 class String
 {
 public:
 	String();
+	String(char*);
 	~String();
 private:
 	std::allocator<char> alloc;
@@ -14,3 +16,24 @@ private:
 	char *elements;
 	char *first_free;
 };
+
+String::String()
+{
+	char* p = alloc.allocate(1);
+	alloc.construct(p, '\0');
+}
+
+String::String(char* cp)
+{
+	size_t n = strlen(cp) + 1;
+	char* p = alloc.allocate(n);
+
+	for(int i = 0; i < n; ++i)
+		alloc.construct(p+i, *(cp+i));
+	// std::for_each(cp, cp+n, [](char c){ allocate.construct(p, c); })
+}
+
+String::~String()
+{
+	
+}
