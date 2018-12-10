09223a83f7d90023f179c36a64a45890c52c5ad9
tangxiang <tangxiang007@gmail.com>
2018-12-10 05:10:12 -0800

    add ex22.cpp

---
+++ ch14_Overloaded_Operations_and_Conversions/ex22.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "Sales_data_ex22.h"
+
+int main()
+{
+    Sales_data sales_data1("001-01", 1, 100);
+    Sales_data sales_data2;
+    Sales_data sales_data3("001-02");
+    Sales_data sales_data4(std::cin);
+    std::cout << sales_data4 << std::endl;
+
+    return 0;
+}
