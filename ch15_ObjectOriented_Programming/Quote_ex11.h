acc193b31f9de18de7adaa9d3fa82fa81dde2396
tangxiang <tangx@amyrobotics.com>
2019-02-01 18:06:18 +0800

    update Quote_ex11.h

--- ch15_ObjectOriented_Programming/Quote_ex11.h
+++ ch15_ObjectOriented_Programming/Quote_ex11.h
@@ -9,7 +9,8 @@ public:
 	Quote() = default;
 	Quote(const std::string &book, double sales_price) : bookNo(book), price(sales_price) {}
 	std::string isbn() const { return bookNo; }
-	virtual double net_price(std::size_t n) const { return n * price; } 
+	virtual double net_price(std::size_t n) const { return n * price; }
+	virtual debug();
 	virtual ~Quote() = default;
 private:
 	std::string bookNo;
