1c0005bfe489a6428cb7558b44fa26e954180a35
tangxiang <tangxiang007@gmail.com>
2019-05-07 09:04:37 -0700

    update ex04.cpp

--- ch17_Specialized_Library_Facilities/ex04/ex04.cpp
+++ ch17_Specialized_Library_Facilities/ex04/ex04.cpp
@@ -32,33 +32,52 @@ std::vector<matches> findBook(const std::vector<std::vector<Sales_data>> &files,
 	std::vector<matches> ret;
 	for(auto it = files.cbegin(); it != files.cend(); ++it)
 	{
-		auto found = equal_range(it->cbegin(), it->cend(), book, compareIsbn);
+		auto found = std::equal_range(it->cbegin(), it->cend(), Sales_data(book), compareIsbn);
 		if(found.first != found.second)
-			ret.push_back(make_tuple(it - files.cbegin(), found.first, found.second));
+			ret.push_back(std::make_tuple(it - files.cbegin(), found.first, found.second));
 	}
 	return ret;
 }
 
-
+void reportResults(std::istream &in, std::ostream &os, const std::vector<std::vector<Sales_data>> &files)
+{
+	std::string s;
+	while(in >> s)
+	{
+		auto trans = findBook(files, s);
+		if(trans.empty())
+		{
+			std::cout << s << " not found in any stores" << std::endl;
+			continue;
+		}
+		for(const auto &store : trans)
+			os << "store " << std::get<0>(store) << " sales: " << std::accumulate(std::get<1>(store), std::get<2>(store), Sales_data(s)) << std::endl;
+	}
+}
 
 int main()
 {
     Sales_data sales_data1("001-01", 1, 100);
-    Sales_data sales_data2;
-    Sales_data sales_data3("001-02");
-    // Sales_data sales_data4(std::cin);
-    // std::cout << sales_data4 << std::endl;
-    std::cout << std::hex << std::hash<std::string>()("001-01") << std::endl;
-    std::cout << std::hex << std::hash<unsigned>()(1) << std::endl;
-    std::cout << std::hex << std::hash<double>()(100) << std::endl;
+    Sales_data sales_data2("001-01", 2, 100);
+    Sales_data sales_data3("001-02", 2, 80);
 
-    std::unordered_multiset<Sales_data> SDset;
-    SDset.emplace(sales_data1);
-    SDset.emplace("001-03", 1, 200);
-    SDset.emplace(sales_data3);
+    std::vector<Sales_data> vs1 = {sales_data1, sales_data3};
+    std::vector<Sales_data> vs2 = {sales_data2};
+    std::vector<std::vector<Sales_data>> vvs;
 
-	for(const auto &item : SDset)
-		std::cout << "the hash code of " << item.isbn() <<":\n0x" << std::hex << std::hash<Sales_data>()(item) << "\n";
+    reportResults(std::cin, std::cout, vvs);
+
+ //    std::cout << std::hex << std::hash<std::string>()("001-01") << std::endl;
+ //    std::cout << std::hex << std::hash<unsigned>()(1) << std::endl;
+ //    std::cout << std::hex << std::hash<double>()(100) << std::endl;
+
+ //    std::unordered_multiset<Sales_data> SDset;
+ //    SDset.emplace(sales_data1);
+ //    SDset.emplace("001-03", 1, 200);
+ //    SDset.emplace(sales_data3);
+
+	// for(const auto &item : SDset)
+	// 	std::cout << "the hash code of " << item.isbn() <<":\n0x" << std::hex << std::hash<Sales_data>()(item) << "\n";
 
     return 0;
 }
