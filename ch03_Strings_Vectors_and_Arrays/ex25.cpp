62059c9ff141f09e0b0ec8a003dbed60f6d9612d
tangxiang <tangxiang007@gmail.com>
2019-05-23 09:02:33 -0700

    update ex25.cpp, add input and output code.

--- ch03_Strings_Vectors_and_Arrays/ex25.cpp
+++ ch03_Strings_Vectors_and_Arrays/ex25.cpp
@@ -13,8 +13,8 @@ int main()
     auto it = score.begin();
     while(cin >> grade)
     {
-        (*(it + grade/10))++;
-        cout << grade/10 << endl;
+        ++(*(it + grade/10));
+        // cout << grade/10 << endl;
     }
     for(auto e : score)
     {
@@ -23,3 +23,7 @@ int main()
     cout << endl;
     return 0;
 }
+//input:
+//42 65 95 100 39 67 95 76 88 76 83 92 76 93
+//output:
+// 0 0 0 1 1 0 2 3 2 4 1
