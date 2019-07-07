e421c7989b35aa787f47262bc680142294c6f890
tangxiang <tangxiang007@gmail.com>
2019-07-07 05:20:21 -0700

    rm dir 0make, dir slamtest, dir xunwei-linuxC

--- 0make/main/haha.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int haha(char name[15])
-{
-	printf ("\n\nHi, Dear %s, nice to meet you.", name);
-}
--- xunwei-linuxC/19_driver_hello/.driver_hello_module.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/txvm-ubuntu64/cpplearn/19_driver_hello/driver_hello_module.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/txvm-ubuntu64/cpplearn/19_driver_hello/driver_hello_module.ko /home/txvm-ubuntu64/cpplearn/19_driver_hello/driver_hello_module.o /home/txvm-ubuntu64/cpplearn/19_driver_hello/driver_hello_module.mod.o
