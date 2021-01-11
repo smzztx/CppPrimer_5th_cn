7b13a7601b7f774b07e5739e11c25dbe53333d38
tangxiang <tangx@amyrobotics.com>
2021-01-11 11:51:09 +0800

    update ex58.cpp, rm the class name of vecSize

--- ch07_Classes/ex58.cpp
+++ ch07_Classes/ex58.cpp
@@ -3,7 +3,8 @@
 #include <vector>
 //you should  initialize a static atributte of a class in main, instead of the .cpp of the class, or just after the class definition in theglobal scope of the .h
 double Example::rate = 6.5;
-std::vector<double> Example::vec(Example::vecSize);
+std::vector<double> Example::vec(vecSize);
+// std::vector<double> Example::vec(Example::vecSize);
 
 int main()
 {
