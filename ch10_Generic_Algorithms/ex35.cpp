86e561e0ca4863bc64791c6fe9383aa395cb875f
tangxiang <tangx@amyrobotics.com>
2023-02-09 14:47:47 +0800

    fixed #23.

--- ch10_Generic_Algorithms/ex35.cpp
+++ ch10_Generic_Algorithms/ex35.cpp
@@ -8,8 +8,8 @@ int main()
 {
 	vector<int> v1 = {1,2,3,4,5,6,7,8};
 
-	for(auto iter = v1.end()-1; iter != v1.begin()-1; --iter)
-		cout << *iter << " ";
+	for(auto iter = v1.end(); iter != v1.begin(); )
+		cout << *--iter << " ";
 	cout << endl;
 
 	return 0;
